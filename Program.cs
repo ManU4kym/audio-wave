3277b0328a21c873db1f44bd8e101579e8964e93
@Versatilekim <emmannjoroge36@gmail.com>
2024-11-27 11:03:20 +0300

    Song path

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main()
     {
         // Initialize the Audio File Reader for MP3
-        using (var audioFile = new AudioFileReader("C:\\Users\\emman\\Downloads\\Music\\Chris-Brown-Tempo-Audio-(NaijaRemix.Com).mp3"))
+        using (var audioFile = new AudioFileReader("\\Downloads\\Music\\Chris-Brown-Tempo-Audio-(NaijaRemix.Com).mp3"))
         using (var outputDevice = new WaveOutEvent())
         {
             // Set the output device and play the file
